448f5c94a7a51db54647baf20f14bdf192d1271e
jackson-deusdeth <jackson@ziromoda.com>
2020-04-14 15:31:35 -0300

    ada

--- index.html/new.js
+++ index.html/new.js
@@ -5,16 +5,15 @@ function acesso(form) {
 
     form.nome.value = form.nome.value.toLowerCase()
     form.senha.value = form.senha.value.toLowerCase()
-
+    console.log(form.nome.value)
     if (form.nome.value == "Maria" && form.senha.value == "123" ||
         form.nome.value == "Evelin" && form.senha.value == "456" ||
         form.nome.value == "Wesley" && form.senha.value == "789" ||
         form.nome.value == "Jackson" && form.senha.value == "321")
-      { location = "acesso.html" }
+      { location = "formula.html" }
 
     else {
         form.nome.value = ""
         form.senha.value = ""
         alert("Dados Incorretos")
-    }
-}
+    }}
